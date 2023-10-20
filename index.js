4bc371d2b50c56cccec77df87875bf19f542225f
swapnadeepmohapatra <swapnadeepmohapatra@yahoo.com>
2023-10-21 00:51:56 +0530

    [Added] Named Hello

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/:name", (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
